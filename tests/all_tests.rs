1e75b5bd1acbdb901d9f656f014b5db1500e6bd0
Aaron Li <a1li@ucsd.edu>
2023-05-25 18:27:41 -0700

    Create a set of tests for garbage collection
    The program creates a number of vectors where the sizes and amount
    depends on the input, and the tests try different inputs with a small
    heap size.

--- tests/all_tests.rs
+++ tests/all_tests.rs
@@ -24,6 +24,34 @@ success_tests! {
         file: "linked_list_manipulations.snek",
         expected: "1\n2\n3\n4\n5\n5\n4\n3\n2\n1\nnil"
     },
+    {
+        name: make_vecs_5_succ_0,
+        file: "make_vecs.snek",
+        input: "0",
+        heap_size: 5,
+        expected: "[]",
+    },
+    {
+        name: make_vecs_5_succ_1,
+        file: "make_vecs.snek",
+        input: "1",
+        heap_size: 5,
+        expected: "[]\n[1]",
+    },
+    {
+        name: make_vecs_5_succ_2,
+        file: "make_vecs.snek",
+        input: "2",
+        heap_size: 5,
+        expected: "[]\n[1]\n[1, 2]",
+    },
+    {
+        name: make_vecs_5_succ_3,
+        file: "make_vecs.snek",
+        input: "3",
+        heap_size: 5,
+        expected: "[]\n[1]\n[1, 2],\n[1, 2, 3]",
+    },
 }
 
 runtime_error_tests! {
@@ -39,7 +67,21 @@ runtime_error_tests! {
         file: "vec_get.snek",
         input: "5",
         expected: "",
-    }
+    },
+    {
+        name: make_vecs_5_oom_4,
+        file: "make_vecs.snek",
+        input: "4",
+        heap_size: 5,
+        expected: "out of memory",
+    },
+    {
+        name: make_vecs_5_oom_5,
+        file: "make_vecs.snek",
+        input: "5",
+        heap_size: 5,
+        expected: "out of memory",
+    },
 }
 
 static_error_tests! {}
